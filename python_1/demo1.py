d4840430ec7c15ad5cf3586912d21131d1ee999b
aicouncil <56471284+aicouncil@users.noreply.github.com>
2025-05-03 16:00:57 +0530

    Add files via upload

---
+++ python_1/demo1.py
@@ -0,0 +1,15 @@
+#python code editor
+
+'''
+This is a comment
+which is in multi-line format.
+Here we are learning how to use python scripting.
+'''
+
+print("Hello World")
+
+name = 'Ram'
+print("Hello" , name)
+
+age = 20
+print("The age is" , age + 15)
